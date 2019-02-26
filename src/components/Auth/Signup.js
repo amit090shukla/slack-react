e6d548cd8c060dd6205856691c1b8eb796343e25
Amit Shukla <amit090shukla@gmail.com>
2019-02-25 23:38:03 -0800

    Signup page

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,7 +1,145 @@
 import React, { Component } from "react";
+import "../App.css";
+import firebase from "../../firebase";
+import {
+  Grid,
+  Form,
+  Segment,
+  Button,
+  Header,
+  Message,
+  Icon
+} from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 export default class Signup extends Component {
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirm: "",
+    errors: []
+  };
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+  handleSubmit = event => {
+    const { email, password } = this.state;
+    if (this.isFormValid()) {
+      event.preventDefault();
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(user => console.log(user))
+        .catch(err => console.log(err));
+    }
+  };
+  isFormEmpty = ({ username, email, password, passwordConfirm }) => {
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirm.length
+    );
+  };
+  isPasswordValid = ({ password, passwordConfirm }) => {
+    if (password.length < 6 || passwordConfirm.length < 6) {
+      return false;
+    } else if (password !== passwordConfirm) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+  isFormValid = () => {
+    let errors = [];
+    let error;
+    if (this.isFormEmpty(this.state)) {
+      error = { message: "Fill in all the fields" };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else if (!this.isPasswordValid(this.state)) {
+      error = {
+        message: "Passwords are invalid"
+      };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  displayError = errors =>
+    errors.map((error, key) => <p key={key}>{error.message}</p>);
   render() {
-    return <div>Register</div>;
+    const { username, password, email, passwordConfirm, errors } = this.state;
+    return (
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="green" textAlign="center">
+            <Icon name="puzzle piece" color="green" />
+            Register to React Slack
+          </Header>
+          <Form size="large" onSubmit={this.handleSubmit}>
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                value={username}
+                onChange={this.handleChange}
+                type="text"
+              />
+              <Form.Input
+                fluid
+                name="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email Address"
+                value={email}
+                onChange={this.handleChange}
+                type="email"
+              />
+              <Form.Input
+                fluid
+                name="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                value={password}
+                onChange={this.handleChange}
+                type="password"
+              />
+              <Form.Input
+                fluid
+                name="passwordConfirm"
+                icon="repeat"
+                iconPosition="left"
+                placeholder="Retype Password"
+                value={passwordConfirm}
+                onChange={this.handleChange}
+                type="password"
+              />
+              <Button color="green" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          {errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayError(errors)}
+            </Message>
+          )}
+          <Message>
+            Already a user ? <Link to="/login">Login</Link>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
